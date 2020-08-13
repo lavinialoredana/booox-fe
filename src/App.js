85f5d1982ac50b5ecfd9e1fab1d7f33dd007d141
roshanislimitless <roshan.sapkota2016@gmail.com>
2020-08-13 19:40:20 +0200

    this is the first step

--- src/App.js
+++ src/App.js
@@ -1,24 +1,20 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import styled from 'styled-components';
+import Header from './components/Header';
+
+const StyledDiv = styled.div`
+  width:30px
+
+
+
+`;
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="main-app">
+      <Header />
+
     </div>
   );
 }
---
+++ src/components/Header.js
@@ -0,0 +1,14 @@
+import React from "react";
+import css from '../App.css';
+
+
+const Header = ( ) => {
+    return (
+        <div className='header'>
+            <h1 className="header-text"> This is the header </h1>
+        </div>
+    )
+}
+
+export default Header;
+
