6bad79199a16a29b4a3839fd185e51fc35b2cdd7
umitao <umitao@gmail.com>
2020-08-24 18:55:42 +0300

    no changes

--- src/components/SearchResults.js
+++ src/components/SearchResults.js
@@ -1,71 +1,56 @@
-import React, {useState, useEffect} from  'react';
-import '../App.css';
+import React, { useState, useEffect } from "react";
+import "../App.css";
 import Search from "./Search";
 
+const SearchResults = () => {
+  const [finalSearchValue, setFinalSearchValue] = useState("");
+  console.log(finalSearchValue);
 
+  const [fetchedData, setFetchedData] = useState([]);
 
+  useEffect(() => {
+    fetch("/search")
+      .then((res) => res.json())
+      .then((result) => setFetchedData(result))
+      .catch((error) => console.error(error));
+  }, []);
 
-const SearchResults = ( ) =>{
+  console.log(fetchedData);
 
-    const [finalSearchValue, setFinalSearchValue ] = useState("");
- console.log(finalSearchValue);
+  finalSearchValue.toLowerCase();
 
-    const [fetchedData, setFetchedData] = useState([]);
+  const filtered = fetchedData.filter(
+    (any) =>
+      any.author.toLowerCase().includes(finalSearchValue) ||
+      any.title.toLowerCase().includes(finalSearchValue) ||
+      any.language.toLowerCase().includes(finalSearchValue) ||
+      any.publisher.toLowerCase().includes(finalSearchValue) ||
+      any.title.toLowerCase().includes(finalSearchValue)
+  );
+  console.log("filtered", filtered);
 
-
-   useEffect(( ) =>{
-
-    fetch('/search')
-    .then(res => res.json())
-    .then(result => setFetchedData(result))
-    .catch(error => console.error(error))
-
-
-   }, [])
-
-   console.log(fetchedData);
-
-   finalSearchValue.toLowerCase();
-   
-
-   const filtered = fetchedData.filter((any) => any.author.toLowerCase().includes(finalSearchValue) || any.title.toLowerCase().includes(finalSearchValue) || any.language.toLowerCase().includes(finalSearchValue) || any.publisher.toLowerCase().includes(finalSearchValue) || any.title.toLowerCase().includes(finalSearchValue));
-   console.log("filtered", filtered);
-
-  
-   
-
-
- 
-
-
-
-
-    
-
-
-    return (
-        <div>
-            <Search search={setFinalSearchValue}/>
-            {filtered ? 
-            filtered.map(any => {
-                return (
-                    <div className="single-book-details">
-                    <div >{any.title}</div>
-                    <div >{any.author}</div>
-                    <div >{any.published_date}</div>
-                    <div >{any.publisher}</div>
-                    <div >{any.subtitle}</div>
-                    <div >{any.language}</div>
-                    <div >{any.isbn}</div>
-                    </div>
-                )
-            })
-             
-             
-             
-            :  <div>Do search with title, bookname,  </div>}
-        </div>
-    )
-}
+  return (
+    <div>
+      <Search search={setFinalSearchValue} />
+      {filtered ? (
+        filtered.map((any) => {
+          return (
+            <div className="single-book-details">
+              <div>{any.title}</div>
+              <div>{any.author}</div>
+              <div>{any.published_date}</div>
+              <div>{any.publisher}</div>
+              <div>{any.subtitle}</div>
+              <div>{any.language}</div>
+              <div>{any.isbn}</div>
+            </div>
+          );
+        })
+      ) : (
+        <div>Do search with title, bookname, </div>
+      )}
+    </div>
+  );
+};
 
 export default SearchResults;
