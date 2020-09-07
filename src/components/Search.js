d817d95dcd62da211fa2dcce62e5604144dc9c12
umitao <umitao@gmail.com>
2020-09-07 18:47:46 +0300

    fixed some css

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,47 +1,38 @@
-import React, {useState} from  'react';
-import '../App.css';
-import Button from './Button';
+import React, { useState } from "react";
+import "../App.css";
+import Button from "./Button";
 
+const Search = (props) => {
+  const [inputValue, setInputValue] = useState("");
+  const [inputValue1, setInputValue1] = useState("");
 
+  const handleOnChange = (event) => {
+    const newData = event.target.value;
+    setInputValue(newData);
+  };
 
+  const handleOnSubmit = (event) => {
+    event.preventDefault();
+    props.search(inputValue);
+    setInputValue1("some");
+  };
 
-const Search = (props) =>{
-
-    const [inputValue, setInputValue ] = useState("");
-    const [inputValue1, setInputValue1 ] = useState("");
-
-    const handleOnChange = (event) => {
-        const newData = event.target.value
-       setInputValue(newData);
-      
-
-    }
-
-    const handleOnSubmit = (event) => {
-        event.preventDefault();
-        props.search(inputValue);
-        setInputValue1("some")
-       
-    }
-    
-
-
-    return (
-        <div  className={inputValue1 ? "search search1" : "search"}>
-            <h2>Search Book</h2>
-            <form onSubmit={handleOnSubmit}>
-                <label htmlFor="search"></label>
-                <input
-                type="text"
-                name="search"
-                id="search"
-                onChange={handleOnChange}
-                value={inputValue}/>
-                <Button buttonName="submit"/>
-            </form>
-
-        </div>
-    )
-}
+  return (
+    <div className={inputValue1 ? "search search1" : "search"}>
+      <form onSubmit={handleOnSubmit}>
+        <label htmlFor="search"></label>
+        <input
+          type="text"
+          name="search"
+          id="search"
+          onChange={handleOnChange}
+          value={inputValue}
+          placeholder="Search for Books"
+        />
+        <Button buttonName="submit" />
+      </form>
+    </div>
+  );
+};
 
 export default Search;
--- src/components/SearchResults.js
+++ src/components/SearchResults.js
@@ -2,7 +2,6 @@ import React, { useState, useEffect } from "react";
 import "../App.css";
 import Search from "./Search";
 
-<<<<<<< HEAD
 const SearchResults = () => {
   const [finalSearchValue, setFinalSearchValue] = useState("");
   console.log(finalSearchValue);
@@ -16,7 +15,7 @@ const SearchResults = () => {
       .catch((error) => console.error(error));
   }, []);
 
-  console.log(fetchedData);
+  console.log("fetchedData", fetchedData);
 
   finalSearchValue.toLowerCase();
 
@@ -26,121 +25,54 @@ const SearchResults = () => {
       any.title.toLowerCase().includes(finalSearchValue) ||
       any.language.toLowerCase().includes(finalSearchValue) ||
       any.publisher.toLowerCase().includes(finalSearchValue) ||
-      any.title.toLowerCase().includes(finalSearchValue)
+      any.subtitle.toLowerCase().includes(finalSearchValue)
   );
   console.log("filtered", filtered);
 
+  const DisplayAll = () => {
+    if (finalSearchValue === "*") {
+      return fetchedData.map((any) => {
+        return (
+          <div className="single-book-details">
+            <div>{any.title}</div>
+            <div>{any.author}</div>
+            <div>{any.published_date}</div>
+            <div>{any.publisher}</div>
+            <div>{any.subtitle}</div>
+            <div>{any.language}</div>
+            <div>{any.isbn}</div>
+          </div>
+        );
+      });
+    } else {
+      return filtered.map((any) => {
+        return (
+          <div className="single-book-details">
+            <div>Title : {any.title}</div>
+            <div>Author : {any.author}</div>
+            <div>Published date: {any.published_date}</div>
+            <div>Publisher: {any.publisher}</div>
+            <div>Subtitle :{any.subtitle}</div>
+            <div>Language: {any.language}</div>
+            <div>ISBN : {any.isbn}</div>
+          </div>
+        );
+      });
+    }
+  };
+
+  const display = {
+    display: "none",
+  };
+
   return (
-    <div>
-      <Search search={setFinalSearchValue} />
-      {filtered ? (
-        filtered.map((any) => {
-          return (
-            <div className="single-book-details">
-              <div>{any.title}</div>
-              <div>{any.author}</div>
-              <div>{any.published_date}</div>
-              <div>{any.publisher}</div>
-              <div>{any.subtitle}</div>
-              <div>{any.language}</div>
-              <div>{any.isbn}</div>
-            </div>
-          );
-        })
-      ) : (
-        <div>Do search with title, bookname, </div>
-      )}
+    <div className="search-results">
+      <div>
+        <Search search={setFinalSearchValue} />
+      </div>
+      {finalSearchValue ? DisplayAll() : <p>"type something"</p>}
     </div>
   );
 };
 
 export default SearchResults;
-=======
-
-
-
-const SearchResults = ( ) =>{
-
-    const [finalSearchValue, setFinalSearchValue ] = useState("");
- console.log(finalSearchValue);
-
-    const [fetchedData, setFetchedData] = useState([]);
-
-
-   useEffect(( ) =>{
-
-    fetch('/search')
-    .then(res => res.json())
-    .then(result => setFetchedData(result))
-    .catch(error => console.error(error))
-
-
-   }, [])
-
-   console.log("fetchedData", fetchedData);
-
-   finalSearchValue.toLowerCase();
-   
-
-   const filtered = fetchedData.filter((any) => any.author.toLowerCase().includes(finalSearchValue) || any.title.toLowerCase().includes(finalSearchValue) || any.language.toLowerCase().includes(finalSearchValue) || any.publisher.toLowerCase().includes(finalSearchValue) || any.subtitle.toLowerCase().includes(finalSearchValue));
-   console.log("filtered", filtered);
-
-   const DisplayAll = ( ) => {
-       if (finalSearchValue === "*"){
-           return (
-            fetchedData.map(any => {
-                return (
-                    <div className="single-book-details">
-                    <div >{any.title}</div>
-                    <div >{any.author}</div>
-                    <div >{any.published_date}</div>
-                    <div >{any.publisher}</div>
-                    <div >{any.subtitle}</div>
-                    <div >{any.language}</div>
-                    <div >{any.isbn}</div>
-                    </div>
-                )
-            })
-           )
-       }
-       else{ return (
-        filtered.map(any => {
-            return (
-                <div className="single-book-details">
-                <div >Title : {any.title}</div>
-                <div >Author : {any.author}</div>
-                <div >Published date: {any.published_date}</div>
-                <div >Publisher: {any.publisher}</div>
-                <div >Subtitle :{any.subtitle}</div>
-                <div >Language: {any.language}</div>
-                <div >ISBN : {any.isbn}</div>
-                </div>
-            )
-        })
-       )}
-   }
-
-  
-   
-
-
- 
-
-
-const display = {
-    display: "none"
-};
-
-    
-
-
-    return (
-        <div className="search-results">
-            <div ><Search search={setFinalSearchValue} /></div>
-            {finalSearchValue ? DisplayAll() : <p>"type something"</p>}
-        </div>
-    )
-}
-
-export default SearchResults;
->>>>>>> 0036b21fd276b9d74b37f48d88dcac123d59b82c
