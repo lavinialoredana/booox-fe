bc91608c686b57445bc4591366a14dfdb57d3615
umitao <umitao@gmail.com>
2020-09-15 12:32:08 +0300

    changed search component structure

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -1,18 +1,61 @@
-import React from "react";
+import React, { useState } from "react";
 import "../App.css";
-// import Button from "./Button";
 // import Navigation from "./Navigation";
 import SearchResults from "./SearchResults";
+import searchIcon from "./fotos/search.png";
 
-const HomePage = (props) => {
+const HomePage = () => {
+  const [inputValue, setInputValue] = useState("");
+  const [searchResults, setSearchResults] = useState([]);
+
+  const handleInput = (event) => {
+    setInputValue(event.target.value);
+  };
+
+  const handleSearch = (event) => {
+    event.preventDefault();
+
+    fetch(`/search?q=${inputValue}`)
+      .then((res) => res.json())
+      .then((result) => setSearchResults(result))
+      .catch((error) => console.error(error));
+
+    setInputValue("");
+  };
+
   return (
     <div className="home-page">
       <div className="home-page-up">
-        <h1>No BOOKS SHOULD EVER LAY IDLE</h1>
+        <h1>NO BOOK SHOULD EVER LAY IDLE</h1>
       </div>
 
       <div className="home-page-bottom">
-        <SearchResults />
+        <div className="form-wrapper">
+          <form className="search-form">
+            <div className="searchbar">
+              <input
+                type="text"
+                name="search"
+                id="search"
+                onChange={handleInput}
+                value={inputValue}
+                placeholder="Search for Books"
+                className="searchbar-input"
+              />
+              <div className="search-image-div">
+                <a href="/search">
+                  {" "}
+                  <img className="voice" alt="" src={searchIcon} />
+                </a>
+              </div>
+            </div>
+
+            <button onClick={handleSearch} type="submit" className="button">
+              Search
+            </button>
+          </form>
+        </div>
+        <SearchResults search={searchResults} />
       </div>
 
       <div className="footer"></div>
--- src/components/SearchResults.js
+++ src/components/SearchResults.js
@@ -1,62 +1,12 @@
-import React, { useState, useEffect } from "react";
+import React from "react";
 import "../App.css";
-import Search from "./Search";
-import Delete from "./Delete";
 
-const SearchResults = () => {
-  const [finalSearchValue, setFinalSearchValue] = useState("");
-
-  const [fetchedData, setFetchedData] = useState([]);
-
-  const [renderOnDelete, setRenderOnDelete] = useState(false);
-
-  useEffect(() => {
-    fetch(`/search?q=${finalSearchValue}`)
-      .then((res) => res.json())
-      .then((result) => setFetchedData(result))
-      .catch((error) => console.error(error));
-  }, [finalSearchValue, renderOnDelete]);
-
-  const changeDeleteState = () => {
-    if (renderOnDelete === false) {
-      setRenderOnDelete(true);
-    } else {
-      setRenderOnDelete(false);
-    }
-    console.log(renderOnDelete);
-  };
-
-  //   const filtered = fetchedData.filter(
-  //     (any) =>
-  //       any.author.toLowerCase().includes(finalSearchValue) ||
-  //       any.title.toLowerCase().includes(finalSearchValue) ||
-  //       any.language.toLowerCase().includes(finalSearchValue) ||
-  //       any.publisher.toLowerCase().includes(finalSearchValue) ||
-  //       any.subtitle.toLowerCase().includes(finalSearchValue)
-  //   );
-  //   console.log("filtered", filtered);
-
-  const DisplayAll = () => {
-    if (finalSearchValue === "") {
-      return fetchedData.map((any) => {
-        return (
+const SearchResults = ({ search }) => {
+  return search.map((any) => {
+    return (
+      <div key={any.id} className="search-results">
+        <div className="search-results-display">
           <div className="single-book-details">
-            <Delete />
-            <div>{any.title}</div>
-            <div>{any.author}</div>
-            <div>{any.published_date}</div>
-            <div>{any.publisher}</div>
-            <div>{any.subtitle}</div>
-            <div>{any.language}</div>
-            <div>{any.isbn}</div>
-          </div>
-        );
-      });
-    } else {
-      return fetchedData.map((any) => {
-        return (
-          <div className="single-book-details">
-            <Delete idToDelete={any.id} reRenderFunction={changeDeleteState} />
             <div>
               <b>Title :</b> {any.title}
             </div>
@@ -70,8 +20,7 @@ const SearchResults = () => {
               <b>Publisher:</b> {any.publisher}
             </div>
             <div>
-              <b>Subtitle :</b>
-              {any.subtitle}
+              <b>Subtitle :</b> {any.subtitle}
             </div>
             <div>
               <b>Language:</b> {any.language}
@@ -81,17 +30,10 @@ const SearchResults = () => {
               {any.isbn}
             </div>
           </div>
-        );
-      });
-    }
-  };
-
-  return (
-    <div className="search-results">
-      <Search search={setFinalSearchValue} />
-      <div className="search-results-display">{DisplayAll()}</div>
-    </div>
-  );
+        </div>
+      </div>
+    );
+  });
 };
 
 export default SearchResults;
