3459e35eef86768ab7272f516d6c2d0c2e5ddaaf
umitao <umitao@gmail.com>
2020-09-28 22:43:54 +0300

    moved search to home

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -1,48 +1,94 @@
-import React, { Fragment } from "react";
+import React, { Fragment, useState, useEffect } from "react";
 import "./HomePage.css";
-import image from './fotos/5.jpg';
-import image1 from './fotos/6.jpg';
+import image from "./fotos/5.jpg";
+import image1 from "./fotos/6.jpg";
 
 import SearchResults from "./SearchResults";
 
+const HomePage = () => {
+  const [inputValue, setInputValue] = useState("");
+  const [fetchedData, setFetchedData] = useState([]);
 
-const HomePage = (props) => {
-  // const [classNam, setClassname] = useState(false);
+  const handleOnChange = (event) => {
+    setInputValue(event.target.value);
+  };
 
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    fetch(`/search?q=${inputValue}`)
+      .then((res) => res.json())
+      .then((result) => {
+        setFetchedData(result);
+      })
+      .catch((error) => console.error(error));
+  };
   return (
-    <Fragment>    <div className="home-page">
-      <div className="home-page-header">
-        <h1>NO BOOK</h1>
-        <h1>SHOULD EVER LAY</h1>
-        <h1>IDLE</h1>
-      </div>
+    <Fragment>
+      {" "}
+      <div className="home-page">
+        <div className="home-page-header">
+          <h1>NO BOOK</h1>
+          <h1>SHOULD EVER LAY</h1>
+          <h1>IDLE</h1>
+        </div>
 
-      <div className="home-page-search">
-            
-            <SearchResults />
-      </div>
+        <div className="home-page-search">
+          <div className="search-form-container">
+            <form onSubmit={handleSubmit} className="search-bar">
+              <input
+                type="text"
+                name="search"
+                id="search"
+                onChange={handleOnChange}
+                value={inputValue}
+                placeholder="FIND YOUR NEXT BOOK ( BOOKS, AUTHORS, LANGUAGE, CATEGORY … )"
+                className="searchbar-input"
+              />
 
-      
-    </div>
-    <div className="lower-body">
-      <div className="lower-body-child-1">
-        <div className="lower-body-child-1-child lower-body-child-1-child-1"><h3>HOW DOES IT</h3><h3>WORK?</h3></div>
-        <div className="lower-body-child-1-child lower-body-child-1-child-2"><img className="lower-body-image" src={image}></img></div>
-        <div className="lower-body-child-1-child lower-body-child-1-child-3">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec sit amet finibus tellus. Proin id dapibus libero. Vivamus ac sem at urna dignissim consequat.</div>
-
+              <button>Search</button>
+            </form>
+          </div>
+          {fetchedData && <SearchResults data={fetchedData} />}
+        </div>
       </div>
-      <div className="lower-body-child-2">
-        <div className="lower-body-child-2-child lower-body-child-2-child-1"><img className="lower-body-image" src={image1}></img></div>
-        <div className="lower-body-child-2-child lower-body-child-2-child-2">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec sit amet finibus tellus. Proin id dapibus libero. Vivamus ac sem at urna dignissim consequat.</div>
-        </div>      
-    </div>
-    <div className="footer">
-      <h2 className="footer-child footer-child-h2" >CONTACT</h2>
-      <div className="footer-child-2"><h2 className="footer-child">Phone</h2><p> 0034 697 874 032</p></div>
-      <div className="footer-child-3"><h2 className="footer-child">E-mail</h2><p>roshan.sapkota2016@gmail.com</p></div>
-    </div>
+      <div className="lower-body">
+        <div className="lower-body-child-1">
+          <div className="lower-body-child-1-child lower-body-child-1-child-1">
+            <h3>HOW DOES IT</h3>
+            <h3>WORK?</h3>
+          </div>
+          <div className="lower-body-child-1-child lower-body-child-1-child-2">
+            <img className="lower-body-image" src={image}></img>
+          </div>
+          <div className="lower-body-child-1-child lower-body-child-1-child-3">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec sit
+            amet finibus tellus. Proin id dapibus libero. Vivamus ac sem at urna
+            dignissim consequat.
+          </div>
+        </div>
+        <div className="lower-body-child-2">
+          <div className="lower-body-child-2-child lower-body-child-2-child-1">
+            <img className="lower-body-image" src={image1}></img>
+          </div>
+          <div className="lower-body-child-2-child lower-body-child-2-child-2">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec sit
+            amet finibus tellus. Proin id dapibus libero. Vivamus ac sem at urna
+            dignissim consequat.
+          </div>
+        </div>
+      </div>
+      <div className="footer">
+        <h2 className="footer-child footer-child-h2">CONTACT</h2>
+        <div className="footer-child-2">
+          <h2 className="footer-child">Phone</h2>
+          <p> 0034 697 874 032</p>
+        </div>
+        <div className="footer-child-3">
+          <h2 className="footer-child">E-mail</h2>
+          <p>roshan.sapkota2016@gmail.com</p>
+        </div>
+      </div>
     </Fragment>
-
   );
 };
 
--- src/components/SearchResults.js
+++ src/components/SearchResults.js
@@ -1,119 +1,47 @@
-import React, { useState, useEffect } from "react";
+import React from "react";
 import { Link } from "react-router-dom";
 import "../App.css";
-import Search from "./Search";
 
-const SearchResults = () => {
+const SearchResults = (props) => {
   const Heading = [
     "OWNER",
     "AUTHOR",
     "BOOK TITLE",
     "LANGUAGE",
-    "VIEW / Borrow"
+    "VIEW / Borrow",
   ];
-  const [finalSearchValue, setFinalSearchValue] = useState("");
-
-  const [fetchedData, setFetchedData] = useState([]);
-
- // const [renderOnDelete, setRenderOnDelete] = useState(false);
-
-  const [preloadedResults, setPreloadedResults] = useState([]);    
-
-  useEffect(() => {
-    fetch(`/books`)
-      .then((res) => res.json())
-      .then((result) => setPreloadedResults(result))
-      .catch((error) => console.error(error));
-  }, []);
-
-  
-
-  useEffect(() => {
-    fetch(`/search?q=${finalSearchValue}`)
-      .then((res) => res.json())
-      .then((result) => setFetchedData(result))
-      .catch((error) => console.error(error));
-  }, [finalSearchValue]);
-  
-  console.log(fetchedData);
-
-
-  // const changeDeleteState = () => {
-  //   if (renderOnDelete === false) {
-  //     setRenderOnDelete(true);
-  //   } else {
-  //     setRenderOnDelete(false);
-  //   }
-  //   console.log(renderOnDelete);
-  // };
-
-  const resultRender = ( ) => {
-    if (fetchedData.length > 0) {
-      return <div className="search-results-table-container">
+  return props.data && props.data.length !== 0 ? (
+    <div className="search-results-table-container">
       <table className="table">
-    <thead>
-      <tr className="table-th-tr">
-        {Heading.map((any, index) => (
-          <td key={index}>{any}</td>
-        ))}
-      </tr>
-    </thead>
+        <thead>
+          <tr className="table-th-tr">
+            {Heading.map((any, index) => (
+              <td key={index}>{any}</td>
+            ))}
+          </tr>
+        </thead>
 
-    <tbody>
-    {fetchedData.map( (any) => {
-      return (<tr key={any.id} className="table-tb-tr">
-      <td className="table-td"> {any.title}</td>
-      <td> {any.author}</td>
-      <td> {any.subtitle}</td>
-      <td> {any.language}</td>
-      <td>
-        <Link to={"/book/" + any.id}>
-             <button>Open</button>
-       </Link></td>
-       
-        </tr>)
-
-    })}
-    
-    </tbody>
-  </table>
-  </div>
-    }
-  else {return <div>please enter another keyword</div>}
-    
-  }
-
-  //   const filtered = fetchedData.filter(
-  //     (any) =>
-  //       any.author.toLowerCase().includes(finalSearchValue) ||
-  //       any.title.toLowerCase().includes(finalSearchValue) ||
-  //       any.language.toLowerCase().includes(finalSearchValue) ||
-  //       any.publisher.toLowerCase().includes(finalSearchValue) ||
-  //       any.subtitle.toLowerCase().includes(finalSearchValue)
-  //   );
-  //   console.log("filtered", filtered);
-
-  return (
-    <div className="home-page-search-results">
-      
-      <Search search={setFinalSearchValue} />
-       
-          {/* <Delete idToDelete={any.id} reRenderFunction={changeDeleteState} /> */}
-          <div className="motto-container">
-            <div className="motto-container-child">
-            <h3>Exchange books. Real books.</h3>
-            <h3>The ones with scribbles, dust and wink!</h3>
-            <h3>bookmarks!</h3>
-            </div>
-            </div>
-         <div className="results-status">{finalSearchValue ?  <h2 >/ RESULTS</h2> :  <h2 className="results-status">/ BOOKS </h2>}</div>
-         { fetchedData.length > 0 ? resultRender() : null }
-         <div>
-         {!finalSearchValue ? preloadedResults.map(any => {
-          return <li key={any.id} className="search-default-books-div">•&nbsp;&nbsp;{any.title} - {any.author}, {any.language}</li>
-         }) : null}
-         </div>
+        <tbody>
+          {props.data.map((any) => {
+            return (
+              <tr key={any.id} className="table-tb-tr">
+                <td className="table-td"> {any.title}</td>
+                <td> {any.author}</td>
+                <td> {any.subtitle}</td>
+                <td> {any.language}</td>
+                <td>
+                  <Link to={"/book/" + any.id}>
+                    <button>Open</button>
+                  </Link>
+                </td>
+              </tr>
+            );
+          })}
+        </tbody>
+      </table>
     </div>
+  ) : (
+    <div />
   );
 };
 
